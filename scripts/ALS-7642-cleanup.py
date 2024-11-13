1ec9d46e4a3ca6c5fab32502ebdc41cb22a3d4d3
Luke Sikina <lucas.skina@gmail.com>
2024-11-13 14:50:14 -0500

    cleanup

--- scripts/ALS-7642-cleanup.py
+++ scripts/ALS-7642-cleanup.py
@@ -11,20 +11,11 @@ with open('../../inactive_patnums.txt', 'r') as inactives:
     for patnum in inactives:
         patient_nums_to_omit.add(patnum.strip())
 
-
+logging.info('Pulled {s} inactives'.format(s=len(patient_nums_to_omit)))
 with open('ALS-7642.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    logging.info('filtering out1.csv')
-    with open('out1.csv', 'r') as file:
-        csv_reader = csv.reader(file)
-        for cells in csv_reader:
-            if cells[0] in patient_nums_to_omit:
-                logging.info("Ommitting line for patient {p}", p=cells[0])
-            else:
-                writer.writerow(cells)
-
-    logging.info('filtering out.csv')
-    with open('out.csv', 'r') as file:
+    logging.info('filtering bch_data_oct2024.csv')
+    with open('bch_data_oct2024.csv', 'r') as file:
         csv_reader = csv.reader(file)
         for cells in csv_reader:
             if cells[0] in patient_nums_to_omit:
