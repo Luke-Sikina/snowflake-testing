368649d4fefcf008a898dffd34ae357dc4442073
Luke Sikina <lucas.skina@gmail.com>
2024-11-14 10:53:30 -0500

    ALS-7670.py

--- scripts/ALS-7670.py
+++ scripts/ALS-7670.py
@@ -10,9 +10,51 @@ with open('../../uuids.txt', 'r') as uuids_file:
         uuids.add(uuid.strip())
 logging.info('Populated %s uuids', len(uuids))
 
+logging.info('Mapping uuids to patient_nums')
+patients = dict()
 with open('../../UUID_mapping_07Jan2024.csv') as mapping:
     for row in mapping:
         cells = row.split('|')
-        
+        if cells[2] in uuids:
+            patients[cells[1]] = cells[2]
+            logging.info('Added patient mapping %s: %s', cells[1], cells[2])
+logging.info('Mapped %s uuids', len(patients))
+
+logging.info('Connecting to snowflake')
+account = 'ita05443.us-east-1'
+host='ita05443.us-east-1.snowflakecomputing.com'
+user = 'SVC_IRBP00000159_RA'
+password = os.environ.get("SNOWFLAKE_PASSWORD")
+warehouse = 'RESEARCH_COMPUTING_WH'
+database = 'I2B2_PROD'
+schema = 'EDW'
+role = 'ra_i2b2_protocol_role'
+
+# Establish connection to Snowflake
+conn = snowflake.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    account=account,
+    warehouse=warehouse,
+    database=database,
+    schema=schema,
+    role=role,
+    port=443
+)
+f = open('ALS-7670.sql', 'r')
+query = f.read()
+
+with open('ALS-7670.csv', 'w', newline='') as file:
+    try:
+        cur = conn.cursor()
+        cur.execute(query, [patients.values()])
+        results = cur.fetchmany(1000)
+        while len(results) > 0:
+            for row in results:
+                row = row.append(patients[row[0]])
+                file.write(row)
+    finally:
+        conn.close()
 # Run query
 
